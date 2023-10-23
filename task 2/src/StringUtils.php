80ef5dea74cf1c9a029075ffce0c380589509b70
sedusik <sedusova.tanya@yandex.ru>
2023-10-23 15:09:48 +0300

    add task 2

---
+++ task 2/src/StringUtils.php
@@ -0,0 +1,11 @@
+<?php
+
+function capitalize($text)
+{
+    if ($text === '') {
+        return '';
+    }
+    $firstSymbol = strtoupper($text[0]);
+    $restSubstring = substr($text, 1);
+    return "{$firstSymbol}{$restSubstring}";
+}
---
+++ task 2/tests/StringUtilsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require("/home/testirovanie/src/StringUtils.php");
+
+if (capitalize('hello') !== 'Hello') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+if (capitalize('') !== '') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+echo 'Все тесты пройдены!';
